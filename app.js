caff5fa8efec5b484bd359ff4a26f89740dad68b
cheney <457299596@qq.com>
2020-10-18 09:51:36 +0800

    初版

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
+const config = require('config')
 const fs = require('fs')
-const ws = require('ws')
+const WebSocket = require('ws')
 const Client = require('ssh2').Client
 
-const sshConn = new Client()
+// 全局工具
+global.log = require('tracer').colorConsole({ level: config.log.level })
 
-sshConn.on('ready', () => {
-    console.log('Client :: ready')
-    sshConn.shell((err, stream) => {
-        if (err) throw err
-        stream.on('close', () => {
-            console.log('Stream :: close')
-            sshConn.end()
-        }).on('data', (data) => {
-            console.log('OUTPUT: ' + data)
+// WS双工服务
+const wss = new WebSocket.Server({ port: config.termius.port })
+wss.on('connection', (ws, req) => {
+    try {
+        ws.on('message', msg => {
+            log.info(msg)
+
+            const sshConn = new Client()
+            sshConn.on('ready', () => {
+                console.log('Client :: ready')
+                sshConn.shell((err, stream) => {
+                    if (err) throw err
+                    stream.on('close', () => {
+                        console.log('Stream :: close')
+                        sshConn.end()
+                    }).on('data', (data) => {
+                        console.log('OUTPUT: ' + data)
+                        ws.send(data.toString())
+                    })
+                    stream.end('ls -l\nexit\n')
+                    // stream.end('ls -l\nexit\n')
+                })
+            }).connect({
+                host: '101.132.222.146',
+                port: 22,
+                username: 'root',
+                privateKey: fs.readFileSync('/Users/cheney/project/dingstock/ts.pem')
+            })
+
         })
-        stream.end('ls -l\n')
-        // stream.end('ls -l\nexit\n')
-    })
-}).connect({
-    host: '101.132.222.146',
-    port: 22,
-    username: 'root',
-    privateKey: fs.readFileSync('/Users/cheney/project/dingstock/ts.pem')
+        ws.on('close', () => { log.warn('链接关闭') })
+        ws.on('error', () => { log.error('链接错误') })
+    } catch (error) {
+        ws.terminate()
+        log.error(error)
+    }
 })
+log.info(`TERMIUS服务启动【执行环境:${process.env.NODE_ENV},端口:${config.termius.port}】`)
