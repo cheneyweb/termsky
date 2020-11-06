cdce88942def7180ac4df075d6326b48797ad8c7
cheney <457299596@qq.com>
2020-11-06 15:45:51 +0800

    服务完善

--- src/controller/group.js
+++ src/controller/group.js
@@ -8,8 +8,8 @@ const router = new Router()
 router.get('/query', async (ctx, next) => {
     let inparam = ctx.request.query
 
-    let items = await mongodb.collection(inparam.type).find({}, { projection: { group: 1, _id: 0 } }).distinct('group').toArray()
-    let groups = items.map(o => o.group)
+    let items = await mongodb.collection(inparam.type).find({}, { projection: { group: 1, _id: 0 } }).toArray()
+    let groups = [...new Set(items.map(o => o.group))]
 
     ctx.body = { err: false, res: groups }
 })
